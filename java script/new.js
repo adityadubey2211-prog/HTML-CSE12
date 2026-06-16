e8dc4c3d783715ef641531d1deec59c51f4fd3b3
Aditya dubey <adityadubey2211@gmail.com>
2026-06-16 21:23:58 +0530

    Add files via upload

---
+++ java script/new.js
@@ -0,0 +1,11 @@
+// console.log("USER DETAILS");
+// const accountid = "22334455";
+// let name = "Ipshita";
+// var email = "ipshita@google.com";
+// let accountcity = "Hyderabad";
+// console.table([accountid, name, email, accountcity]);
+//ex-1 (notes)
+let name = "aditya";
+const a = 23;
+console.log(name + a);
+console.log(typeof name);
